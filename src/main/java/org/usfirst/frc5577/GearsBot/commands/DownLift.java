d032f71666001a4157e5891c3ddc12f2a423128b
Matt <matteatsleftovers@gmail.com>
2019-01-26 14:48:43 -0500

    Port Cerberus to HellHound project

---
+++ src/main/java/org/usfirst/frc5577/GearsBot/commands/DownLift.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc5577.GearsBot.commands;
+
+import org.usfirst.frc5577.GearsBot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DownLift extends Command {
+	
+	private double speed = 0;
+	
+	public DownLift(double speed) {
+		requires(Robot.lift);
+		this.speed = speed;
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void execute() {
+		Robot.lift.MoveLift(speed);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
